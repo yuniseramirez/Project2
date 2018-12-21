3cc05181029f647987ca8720d9ed4746655f82cd
Yunise Ramirez <yunisemramirez@gmail.com>
2018-12-21 09:01:54 -0500

    Project uploaded

---
+++ Project2/frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <div class= "nav">
+     <router-link to = "/">Home</router-link>
+     <router-link to = "/articles">Articles</router-link>
+     <router-link to = "/photos"> Photos</router-link>
+    <router-view/>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      title: 'People'
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+  text-align: center;
+  color: lightblue;
+ 
+ }
+
+ .nav{ 
+  text-align:center;
+
+  }
+
+</style>
---
+++ Project2/frontend/src/components/Articles.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+   
+    <div class= "data" v-for="article in articles">
+     <img :src="article.Image"/>
+     <h1>  {{article.Title}} </h1>
+     <h3>  {{article.Author}} </h3>
+     <p>  {{article.Paragraph}}</p>
+
+    </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Articles',
+  data () {
+    return {
+     title: 'People',
+      articles: []
+    }
+  },
+
+created: function(){
+
+  fetch('http://localhost:8000/data')
+  .then(r => r.json())
+  .then(json => { 
+
+    this.articles =json;
+  })
+}} 
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  text-align: center;
+  color: lightblue;
+ 
+}
+
+img{ 
+
+  object-fit: cover;
+  width:25% ;
+  height:300px ;
+
+}
+
+.data{
+
+  margin: 25px;
+
+
+
+}
+
+
+
+a {
+  color: black;
+}
+</style>
